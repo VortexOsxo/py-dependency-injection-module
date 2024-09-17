2fdee926ecfe81caf3d6aa377b5a420c64c10a48
Philippe Martin <philippeoliviermartin@gmail.com>
2024-09-17 18:36:27 -0400

    transformed container into a static class

--- lite_py_di/_container/container.py
+++ lite_py_di/_container/container.py
@@ -9,11 +9,11 @@ ServiceType = TypeVar('ServiceType')
 
 class Container(metaclass = SingletonMeta):
     """A container for managing service registration and retrieval."""
-    def __init__(self):
-        self._registry = _Registry()
-        self._instances: Dict[str, object] = {}
+    _registry = _Registry()
+    _instances: Dict[str, object] = {}
 
-    def register(self, class_type: Type, service_config: ServiceConfig):
+    @classmethod
+    def register(cls, class_type: Type, service_config: ServiceConfig):
         """Register a service with the Container.
 
         Args:
@@ -21,42 +21,47 @@ class Container(metaclass = SingletonMeta):
             service_config (ServiceConfig): Configuration for the service.
         """
         register_config = _RegisterConfig(service_config.is_singleton, class_type)
-        self._registry.register(register_config)
+        cls._registry.register(register_config)
 
-    def get(self, class_type: Type[ServiceType]) -> ServiceType:
+    @classmethod
+    def get(cls, class_type: Type[ServiceType]) -> ServiceType:
         """Get an instance of a registered service.
         Args:
             class_type (type): The type of the service class.
         Returns:
             ServiceType: An instance of the service class.
         """
-        return self._get_service_intern(class_type)
+        return cls._get_service_intern(class_type)
 
-    def get_by_name(self, class_name: str) -> ServiceType:
+    @classmethod
+    def get_by_name(cls, class_name: str) -> ServiceType:
         """Get an instance of a registered service by its class name.
         Args:
             class_name (str): The name of the service class.
         Returns:
             ServiceType: An instance of the service class.
         """
-        if not self._registry.is_registered(class_name): raise ServiceNotFound(class_name)
+        if not cls._registry.is_registered(class_name): raise ServiceNotFound(class_name)
 
-        class_type = self._registry.get_class_type(class_name)
-        return self._get_service_intern(class_type)
+        class_type = cls._registry.get_class_type(class_name)
+        return cls._get_service_intern(class_type)
 
-    def _get_service_intern(self, class_type: Type[ServiceType]) -> ServiceType:
-        if not self._registry.is_registered(class_type.__name__): raise UnregisteredService(class_type)
+    @classmethod
+    def _get_service_intern(cls, class_type: Type[ServiceType]) -> ServiceType:
+        if not cls._registry.is_registered(class_type.__name__): raise UnregisteredService(class_type)
         
-        if self._registry._is_singleton(class_type):
-            return self._instances[class_type] if class_type in self._instances else self._create_and_save_service(class_type)
-        return self._create_service(class_type)
+        if cls._registry._is_singleton(class_type):
+            return cls._instances[class_type] if class_type in cls._instances else cls._create_and_save_service(class_type)
+        return cls._create_service(class_type)
 
-    def _create_and_save_service(self, class_type: Type[ServiceType]) -> ServiceType:
-        service = self._create_service(class_type)
-        self._instances[class_type] = service
+    @classmethod
+    def _create_and_save_service(cls, class_type: Type[ServiceType]) -> ServiceType:
+        service = cls._create_service(class_type)
+        cls._instances[class_type] = service
         return service
 
-    def _create_service(self, class_type: Type[ServiceType]) -> ServiceType:
+    @classmethod
+    def _create_service(cls, class_type: Type[ServiceType]) -> ServiceType:
         constructor_args = _get_init_arguments(class_type)
-        args = [self.get(arg) for arg in constructor_args]
+        args = [cls.get(arg) for arg in constructor_args]
         return class_type(*args)
